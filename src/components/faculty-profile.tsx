dc8f810606ceaefbdd6f8070f6deb31aba098353
Itssunil01 <balsunilkumar34@gmail.com>
2025-02-15 00:16:10 +0530

    updated design in faculty page

--- src/components/faculty-profile.tsx
+++ src/components/faculty-profile.tsx
@@ -13,7 +13,7 @@ const FacultyProfile: React.FC<FacultyProfileProps> = ( {faculty} ) => {
           <img
             src={faculty.image_url}
             alt={faculty.name}
-            className="w-32 h-32 rounded-full border-4 border-red-500 shadow-lg mx-auto sm:mx-0"
+            className="w-32 h-32 rounded-full border-2 border-black shadow-lg mx-auto sm:mx-0"
           />
           <div className="text-center sm:text-left mt-4 sm:mt-0">
             <h2 className="text-2xl font-bold text-gray-800">{faculty.name}</h2>
